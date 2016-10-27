d8a82485ab0c3b0816e28b1b72025b0be98b87a1
Will Gallia <wgallia@gmail.com>
2016-10-27 16:12:20 +0000

    small python script to generate vols

---
+++ genvol.py
@@ -0,0 +1,42 @@
+import sys
+from array import array
+
+if len(sys.argv) < 2:
+    z = 16
+else:
+    z = int(sys.argv[1])
+
+dim = { 'x': 16, 'y': 16, 'z': z }
+print(dim)
+volume = [[] for _ in range(dim['z'])]
+
+for i in range(dim['z']):
+    volume[i] = [1 - (i % 2) for j in range(dim['x'] * dim['y'])]
+
+filename = 't%d-%d-%d.py' % (dim['x'], dim['y'], dim['z'])
+
+def pack(slice_contents):
+    """
+    Pack a array of 1|0 into an array of bytes
+    e.g. [1, 0, 0, 0, 0, 0, 0, 1] -> [129]
+    """
+    assert(len(slice_contents) % 8 == 0)
+
+    output = [0 for _ in range(len(slice_contents) // 8)]
+    for i, e in enumerate(slice_contents):
+        assert(e == 0 or e == 1)
+
+        byte_no = i // 8
+        byte_index = i % 8
+        output[byte_no] = output[byte_no] | (e << byte_index)
+
+    return output
+
+
+with open(filename, 'wb') as f:
+    f.write(array('B', [ ord(e) for e in 'VOL' ]).tobytes())
+    f.write(array('I', [ dim[e] for e in 'xyz' ]).tobytes())
+    f.write(array('B', [ ord(e) for e in 'LOV' ]).tobytes())
+
+    for _slice in volume:
+        f.write(array('B', pack(_slice)).tobytes())
